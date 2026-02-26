eab04a5f90504202727c19ea76d9e3d6a3bf2c1a
VtsAshDev <vitorgguimaraes56@gmail.com>
2026-02-26 16:23:30 -0300

    feat(Create): Adiciona o método create para inserir hábitos no banco

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,13 +1,20 @@
 <x-layout>
   <main class="py-10">
-    <h1>
+    <h1 class="font-bold text-4xl text-center mb-4">
       DashBoard
     </h1>
 
-    @auth
-        <p>
-          Bem vindo {{ auth()->user()->name }}
-        </p>
+      <a href="{{ route('habit.create') }}" class="p-2 border-2 bg-white font-bold block">
+        Cadastrar Novo Hábito
+      </a>
+
+    @session('success')
+    <div class="flex">
+      <p class="bg-green-100 border-2 border-green-400 text-green-700 p-3 mb-4">
+        {{ session('success')}}
+      </p>
+    </div>
+    @endsession
 
       <div>
         <h2 class="text-xl mt-4">
@@ -30,12 +37,11 @@
             <p>
               Ainda não há hábitos cadastrados
             </p>
-            <a href="/habito/cadastrar" class="bg-white p-2 border-2">
+            <a href="route('habit.create')" class="bg-white p-2 border-2">
               Cadastre um novo hábito agora
             </a>
           @endforelse
         </ul>
       </div>
-    @endauth
   </main>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HabitController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
@@ -10,7 +11,13 @@ Route::get('/login',[LoginController::class, 'index'])->name('site.login');
 Route::post('/login',[LoginController::class, 'authenticate']);
 Route::get('/register',[RegisterController::class, 'index'])->name('site.register');
 Route::post('/cadastro', [RegisterController::class, 'store'])->name('auth.register');
+
+
 Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard',[SiteController::class, 'dashboard'])->name('site.dashboard');
     Route::post('/logout',[LoginController::class, 'logout'])->name('auth.logout');
+
+    Route::get('/dashboard/habits/create', [HabitController::class, 'create'])->name('habit.create');
+    Route::post('/dashboard/habits', [HabitController::class, 'store'])->name('habit.store');
 });
+
